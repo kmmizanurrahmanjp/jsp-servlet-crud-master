ae0f588fa4de104e67f70dd3926c59b25cad7021
kmmizanurrahmanjp <https.mizan@gmail.com>
2018-09-16 22:22:45 +0600

    Crud application creat

---
+++ src/main/java/com/example/controller/BookController.java
@@ -0,0 +1,128 @@
+package com.example.controller;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.example.dao.BookDao;
+import com.example.model.Book;
+
+/**
+ * Servlet implementation class BookController
+ */
+public class BookController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	private BookDao bookDAO;
+	 
+    public void init() {
+        String jdbcURL = getServletContext().getInitParameter("jdbcURL");
+        String jdbcUsername = getServletContext().getInitParameter("jdbcUsername");
+        String jdbcPassword = getServletContext().getInitParameter("jdbcPassword");
+ 
+        bookDAO = new BookDao(jdbcURL, jdbcUsername, jdbcPassword);
+ 
+    }
+ 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+ 
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getServletPath();
+ 
+        try {
+        	if(action.equals("/new")) {
+        		showNewForm(request, response);
+        	}
+        	if(action.equals("/insert")) {
+        		insertBook(request, response);
+        	}
+        	if(action.equals("/delete")) {
+        		deleteBook(request, response);
+        	}
+        	if(action.equals("/edit")) {
+        		showEditForm(request, response);
+        	}
+        	if(action.equals("/update")) {
+        		updateBook(request, response);
+        	}
+        	if(action.equals("/book")) {
+        		listBook(request, response);
+        	}
+        	if(action.equals("/")) {
+        		RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+                dispatcher.forward(request, response);
+        	}
+        	
+            
+        } catch (SQLException ex) {
+            throw new ServletException(ex);
+        }
+    }
+    
+    private void listBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException, ServletException {
+        List<Book> listBook = bookDAO.listAllBooks();
+        request.setAttribute("listBook", listBook);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("book-list.jsp");
+        dispatcher.forward(request, response);
+    }
+ 
+    private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher dispatcher = request.getRequestDispatcher("book.jsp");
+        dispatcher.forward(request, response);
+    }
+ 
+    private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        Book existingBook = bookDAO.getBook(id);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("book.jsp");
+        request.setAttribute("book", existingBook);
+        dispatcher.forward(request, response);
+ 
+    }
+ 
+    private void insertBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        String title = request.getParameter("title");
+        String author = request.getParameter("author");
+        float price = Float.parseFloat(request.getParameter("price"));
+ 
+        Book newBook = new Book(title, author, price);
+        bookDAO.insertBook(newBook);
+        response.sendRedirect("book");
+    }
+ 
+    private void updateBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String title = request.getParameter("title");
+        String author = request.getParameter("author");
+        float price = Float.parseFloat(request.getParameter("price"));
+ 
+        Book book = new Book(id, title, author, price);
+        bookDAO.updateBook(book);
+        response.sendRedirect("book");
+    }
+ 
+    private void deleteBook(HttpServletRequest request, HttpServletResponse response)
+            throws SQLException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+ 
+        Book book = new Book(id);
+        bookDAO.deleteBook(book);
+        response.sendRedirect("book");
+ 
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<html>
+<head>
+    <title>Books Store Application</title>
+</head>
+<body>
+    <center>
+        <h1>Book Management System</h1>
+        <h2>
+            <a href="book">List of all book</a>
+             
+        </h2>
+    </center>
+    
+</body>
+</html>
